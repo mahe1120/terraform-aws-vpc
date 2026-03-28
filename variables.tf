2309ea2241c568fb543e16d0c30e541942d30953
mahender <gmahe139@gmail.com>
2026-03-28 23:45:03 +0530

    terraaform vpc

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "vpc_cidr"{
+    
+}
+
+variable "enable_dns_hostnames"{
+    default = true
+
+}
+
+variable "project_name"{
+    
+}
+
+variable "environment"{
+
+}
+
+variable "common_tags"{
+    type = map 
+    #default = {}
+}
