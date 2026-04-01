86a00d5e42a04694afd911345802cba23edafa7b
mahender <gmahe139@gmail.com>
2026-04-01 06:38:59 +0530

    vpc module

--- peering.tf
+++ peering.tf
@@ -1,6 +1,7 @@
-resource "aws_vpc_peering_connection" "foo" {
+resource "aws_vpc_peering_connection" "default" {
+  count = var.is_peering_required ? 1 : 0
   vpc_id        = aws_vpc.main.id  # Requestor
-  peer_vpc_id   = data.aws_vpc.default.id # Acceptor
+  peer_vpc_id   = local.default_vpc_id # Acceptor
   auto_accept   = true
 
   tags = merge(
@@ -11,3 +12,34 @@ resource "aws_vpc_peering_connection" "foo" {
     }
   )
   }
+
+  # Creating peer connection to subnets
+  resource "aws_route" "public_subnet_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = local.default_vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
+
+resource "aws_route" "private_subnet_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = local.default_vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
+
+resource "aws_route" "database_subnet_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.database.id
+  destination_cidr_block    = local.default_vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
+
+# Creating peer connection to expense VPC
+resource "aws_route" "default_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = data.aws_route_table.main.route_table_id
+  destination_cidr_block    = var.vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
+
